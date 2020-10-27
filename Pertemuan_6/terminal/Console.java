c28d4d8226fec93c8c609814c2f75951ec84f564
ahmadfadillah220 <71371202+ahmadfadillah220@users.noreply.github.com>
2020-10-27 22:38:49 +0700

    Add files via upload

---
+++ Pertemuan_6/terminal/Console.java
@@ -0,0 +1,16 @@
+package com.terminal;
+
+//visibilitas
+
+public class Console {
+    public static void log(String message){
+        System.out.println(message);
+    }
+}
+
+
+class Terminal {
+    public static void log(String message){
+        System.out.println(message);
+    }
+}
